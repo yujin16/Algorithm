24130a6861b9f3cca35749ffc8602522e0e51c18
yujin16 <bcd772000@naver.com>
2018-08-01 16:29:48 +0900

    1937번 욕심쟁이판다
    dfs와 dp를 이용하여 문제를 푼다
    [37-40] input
    [14-32] map을 돌면서 dp 테이블을 채운다
    상하좌우로 이동할 수 있을 경우(범위 내에 좌표가 있고, 나무수가 이전보다 많은 경우), 새로운 좌표의 dp 값을 이용하여 현재 dp테이블을 채운다.
    새로운 좌표의 dp값이 없을 경우(0인 경우) 재귀를 이용하여 dp 테이블을 채운다.
    이동할 수 있는 상하좌우의 dp 테이블 값중 가장 큰 값에 1을 더하여 현재 좌표의 dp 테이블을 완성한다.

---
+++ Baekjoon/[1937]욕심쟁이판다.cpp
@@ -0,0 +1,49 @@
+/* 1937 욕심쟁이판다()
+ * https://www.acmicpc.net/problem/1937
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int inputN, answer = 0;
+int dir_x[4] = {1, 0, -1, 0};
+int dir_y[4] = {0, 1, 0, -1};
+
+void dfs(vector<vector<int> > &map, vector<vector<int> > &dp, int x, int y) {
+    if(dp[x][y] != 0) return;
+
+    int max_day = 0;
+    dp[x][y] = 1;
+    for (int i = 0; i < 4; i++) {
+        int new_x = x + dir_x[i], new_y = y + dir_y[i];
+        if (new_x >= 0 && new_x < inputN && new_y >= 0 && new_y < inputN) {
+            if(map[new_x][new_y] > map[x][y]) {
+                if(dp[new_x][new_y] == 0)
+                    dfs(map, dp, new_x, new_y);
+                max_day = max(max_day, dp[new_x][new_y]);
+            }
+        }
+    }
+
+    dp[x][y] = max_day + 1;
+    answer = max(answer, dp[x][y]);
+}
+
+int main() {
+    cin >> inputN;
+
+    vector<vector<int> > map(inputN, vector<int>(inputN, 0));
+    for (int i = 0; i < inputN; i++)
+        for (int j = 0; j<inputN; j++)
+            cin >> map[i][j];
+
+    vector<vector<int> > dp(inputN, vector<int>(inputN, 0));
+    for (int i = 0; i < inputN; i++)
+        for (int j = 0; j<inputN; j++)
+            dfs(map, dp, i, j);
+
+    cout << answer << endl;
+    return 0;
+}
