fc41b758f138a290b9c5e75da9a901d20c23ca10
yujin16 <bcd772000@naver.com>
2018-08-10 21:46:21 +0900

    3459번 승자예측하기
    이진 트리를 그려보면 홀수 depth에는 Alice가 짝수 depth에는 Bob이 고를 수 있는 숫자이다.
    승자가 누군지 가려보면, 2-5(Alice) 6-9(Bob) 10-25(Alice) 26-41(Bob) 42-105(Alice)
    즉, N=1일때를 제외하고 Alice4번-Bob4번-Alice16번-Bob16번-Alice64번-Bob64번.... 이런 패턴.
    패턴을 찾긴 했지만.. 더 똑똑한 방법은 못찾겠음...

---
+++ SWExpertAcademy/[3459]승자예측하기.cpp
@@ -0,0 +1,44 @@
+/* 3459 승자예측하기
+ * https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWFPoj1qANoDFAV0&categoryId=AWFPoj1qANoDFAV0&categoryType=CODE
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int Testcase;
+    cin >> Testcase;
+
+    for (int t = 1; t <= Testcase; t++) {
+        int inputN;
+        cin >> inputN;
+
+        int sub = 4;
+        inputN -= 2;
+        string answer = "";
+
+        if(inputN == -1)
+            answer = "Bob";
+        else {
+            while (1){
+                inputN -= sub;
+                if(inputN < 0) {
+                    answer = "Alice";
+                    break;
+                }
+
+                inputN -= sub;
+                if(inputN < 0) {
+                    answer = "Bob";
+                    break;
+                }
+
+                sub *= 4;
+            }
+        }
+
+        cout << "#" << t << " " << answer << endl;
+    }
+    return 0;
+}
