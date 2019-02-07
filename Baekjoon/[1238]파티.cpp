75a87298fa6d697d2dfbbd684a5e29e752280288
yujin16 <bcd772000@naver.com>
2019-02-07 19:43:29 +0900

    1238번 파티
    플로이드와샬 알고리즘으로 풀려고 했으니 시간초과가 뜸. 그래서 다익스트라 알고리즘으로 모든 학생이 x집을 방문하는 시간을 계산하려고 했다.
    > 현재 입력받은 방향으로 x의 집에서 모든 학생의 집까지 가는 최단거리를 구함.
    그리고 방향을 바꾸어(화살표 방향을 바꾼다는 뜻) x의 집에서 모든 학생의 집까지 가는 경로가 결국, 모든 학생의 집에서 x로 가는 경로가 됨.
    > 결국 정방향, 역방향으로 다익스트라를 두번 적용하면 된다.
    > 나는 처음 인접배열 + 큐를 사용했기 때문에 시간도 오래걸리고 메모리를 많이 썼다.
    > 인접리스트 + 우선순위 큐를 이용하여 시간과 메모리를 줄였다.

--- Baekjoon/[1238]파티.cpp
+++ Baekjoon/[1238]파티.cpp
@@ -5,61 +5,48 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
 #include <queue>
 using namespace std;
 
-int inputN, inputM, inputX;
+#define INF 1000000
+int N, M, X, answer = 0;
+
+void Dijkstra(vector<vector<pair<int, int> > > &map, vector<int> &dist) {
+    dist[X] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
+    pq.push(make_pair(0, X));
+    while(!pq.empty()) {
+        int d = pq.top().first, node = pq.top().second;
+        pq.pop();
+
+        int size = map[node].size();
+        for(int i=0; i<size; i++) {
+            int next_node = map[node][i].first, next_dist = d + map[node][i].second;
+            if(next_dist < dist[next_node]) {
+                dist[next_node] = next_dist;
+                pq.push(make_pair(dist[next_node], next_node));
+            }
+        }
+    }
+}
 
 int main() {
-    cin >> inputN >> inputM >> inputX;
-
-    vector<vector<int> > route(inputN + 1, vector<int>(inputN + 1, 0));
-    for (int i = 0; i < inputM; i++) {
-        int start, end, time;
-        cin >> start >> end >> time;
-        route[start][end] = time;
+    vector<vector<pair<int, int> > > goX(1001), goHome(1001);
+    vector<int> goX_dist(1001, INF), goHome_dist(1001, INF);
+    
+    scanf("%d %d %d", &N, &M, &X);
+    for (int i = 0; i < M; i++) {
+        int start, end, t;
+        scanf("%d %d %d", &start, &end, &t);
+        goHome[start].push_back(make_pair(end, t));
+        goX[end].push_back(make_pair(start, t));
     }
+    
+    Dijkstra(goX, goX_dist);
+    Dijkstra(goHome, goHome_dist);
 
-    queue<int> queue;
-
-    queue.push(inputX);
-    vector<int> HometoX(inputN + 1, INT_MAX);
-    HometoX[inputX] = 0;
-    while (!queue.empty()) {
-        int node = queue.front();
-        queue.pop();
-
-        for (int i = 1; i <= inputN; i++) {
-            if(route[i][node] != 0 && HometoX[node] != INT_MAX) {
-                if(HometoX[i] > HometoX[node] + route[i][node]) {
-                    HometoX[i] = HometoX[node] + route[i][node];
-                    queue.push(i);
-                }
-            }
-        }
-    }
-
-    queue.push(inputX);
-    vector<int> XtoHome(inputN + 1, INT_MAX);
-    XtoHome[inputX] = 0;
-    while (!queue.empty()) {
-        int node = queue.front();
-        queue.pop();
-
-        for (int i = 1; i <= inputN; i++) {
-            if(route[node][i] != 0 && XtoHome[node] != INT_MAX) {
-                if(XtoHome[i] > XtoHome[node] + route[node][i]) {
-                    XtoHome[i] = XtoHome[node] + route[node][i];
-                    queue.push(i);
-                }
-            }
-        }
-    }
-
-    int answer = 0;
-    for (int i = 1; i <= inputN; i++)
-        answer = max(answer, HometoX[i] + XtoHome[i]);
+    for (int i = 1; i <= N; i++)
+        answer = max(answer, goX_dist[i] + goHome_dist[i]);
 
     cout << answer << endl;
     return 0;
