7c6ad9e86a0a84f65c131530408528fc9bf595d9
yujin16 <bcd772000@naver.com>
2019-02-12 22:50:53 +0900

    9328번 열쇠
    bfs로 맵을 탐색한다.
    queue에 삽입 : nput의 h,w를 넘지않고 벽(*)이 아닐 때 queue에 삽입한다.
    벽(*) : 현재 위치가 벽(*)인 경우 아무 행동도 하지 않는다.
    공간(.) : 현재 위치가 공간(.)인 경우, 상하좌우를 queue에 삽입한다.
    문서($) : 현재 위치가 문서($)인 경우, answer을 1증가 시키며 상하좌우를 queue에 삽입한다.
    열쇠(a-z) : 현재 위치가 열쇠(a-z)인 경우, key벡터에서 해당하는 key를 true로 변경한다.
    또, door 벡터를 순회하며 맞는 열쇠가 존재하면 해당하는 door 요소를 삭제한다.
    문(A-Z) : 현재 위치가 문(A-Z) 인 경우, 열쇠가 존재하면 상하좌우를 queue에 삽입하고 열쇠가 존재하지 않으면 door 벡터에 추가한다.
    
    > vector에서 array로 변경하고 cin,cout을 scanf, printf로 바꾸니 12ms-> 8ms로 시간단축이 됐다.
    > door를 vector로 push하지 않고 key와 비슷하게 배열로 만들어 접근을 편하게 변경했다.
    > 시간단축은 vector->array가 한몫한 것 같다. 앞으로 되도록이면 array를 사용해야겠다.

--- Baekjoon/[9328]열쇠.cpp
+++ Baekjoon/[9328]열쇠.cpp
@@ -8,81 +8,78 @@
 #include <queue>
 using namespace std;
 
-int inputH, inputW;
-int dir_x[4] = {1, -1, 0, 0};
-int dir_y[4] = {0, 0, 1, -1};
+int T, H, W, answer = 0;
+string map[100];
+bool key[26];
 
-int bfs(vector<vector<char> > &map, vector<bool> &key) {
-    int answer = 0;
+int dir_h[4] = {1, -1, 0, 0};
+int dir_w[4] = {0, 0, 1, -1};
 
+void bfs() {
     queue<pair<int, int> > queue;
-    vector<pair<int, int> > door;
-    for (int i = 0; i < inputH; i++)
-        for (int j = 0; j < inputW; j++)
-            if (i == 0 || i == inputH - 1 || j == 0 || j == inputW - 1)
-                if(map[i][j] != '*')
-                    queue.push(make_pair(i, j));
+    vector<pair<int, int> > door[26];
+
+    for (int h = 0; h < H; ++h)
+        for (int w = 0; w < W; ++w)
+            if (h == 0 || h == H - 1 || w == 0 || w == W - 1)
+                if(map[h][w] != '*')
+                    queue.push(make_pair(h, w));
                     
 
     while(!queue.empty()) {
-        int x = queue.front().first, y = queue.front().second;
+        int h = queue.front().first, w = queue.front().second;
         queue.pop();
 
-        char c = map[x][y];
-        if (c == '*') continue;
-        else if(c == '$') answer++;
+        char c = map[h][w];
+        if (c == '*')       continue;
+        else if(c == '$')   answer++;
         else if ('a' <= c && c <= 'z') {
             key[c - 'a'] = true;
-            for (int i = 0; i < door.size(); i++) {
-                int door_x = door[i].first, door_y = door[i].second;
-                char door_name = map[door_x][door_y];
-                if (key[door_name-'A']) {
-                    door.erase(door.begin() + i--);
-                    queue.push(make_pair(door_x, door_y));
-                }
-                
+            
+            int size = door[c - 'a'].size();
+            for (int i = 0; i < size; ++i) {
+                int dh = door[c - 'a'][i].first, dw = door[c - 'a'][i].second;
+                queue.push(make_pair(dh, dw));
             }
         }
         else if ('A' <= c && c <= 'Z') {
             if(!key[c - 'A']) {
-                door.push_back(make_pair(x, y));
+                door[c - 'A'].push_back(make_pair(h, w));
                 continue;
             }
         }
 
-        map[x][y] = '*';
+        map[h][w] = '*';
         for (int i = 0; i < 4; i++) {
-            int new_x = x + dir_x[i], new_y = y + dir_y[i];
+            int nh = h + dir_h[i], nw = w + dir_w[i];
 
-            if (new_x >= 0 && new_x < inputH && new_y >= 0 && new_y < inputW && map[new_x][new_y] != '*')
-                queue.push(make_pair(new_x, new_y));
+            if (nh >= 0 && nh < H && nw >= 0 && nw < W && map[nh][nw] != '*')
+                queue.push(make_pair(nh, nw));
         }
     }
-    return answer;
 }
 
 int main() {
-    int TestCase;
-    cin >> TestCase;
+    scanf("%d", &T);
+    for (int t = 0; t < T; ++t) {
+        scanf("%d %d", &H, &W);
+        for (int h = 0; h < H; ++h)
+            cin >> map[h];
 
-    for (int t = 0; t < TestCase; t++) {
-        string s;
-        cin >> inputH >> inputW;
+        for (int i = 0; i < 26; ++i)
+            key[i] = false;
 
-        vector<vector<char> > map(inputH, vector<char>(inputW));
-        for (int i = 0; i < inputH; i++) {
-            cin >> s;
-            for (int j = 0; j < inputW; j++)
-                map[i][j] = s[j];
+        string in;
+        cin >> in;
+        if(in != "0") {
+            int size = in.length();
+            for (int i = 0; i < size; ++i)
+                key[in[i] - 'a'] = true;
         }
 
-        cin >> s;
-        vector<bool> key(26, false);
-        if(s!= "0") 
-            for (int i = 0; i < s.length(); i++)
-                key[s[i] - 'a'] = true;
-
-        cout <<  bfs(map, key) << endl;
+        answer = 0;
+        bfs();
+        printf("%d\n", answer);
     }
     return 0;
 }
