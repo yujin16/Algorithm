4227175cb46785ee5636e303fedac8a32c267581
yujin16 <bcd772000@naver.com>
2019-07-15 00:06:31 +0900

    2579번 계단오르기 https://yujin16.github.io/algorithm/algocode/2019/07/15/baekjoon-2579.html

--- Baekjoon/[2579]계단오르기.cpp
+++ Baekjoon/[2579]계단오르기.cpp
@@ -2,35 +2,57 @@
  * https://www.acmicpc.net/problem/2579
  */
 
+// #include <iostream>
+// #include <vector>
+// #define MAX(X,Y) ((X)>(Y)?(X):(Y))
+// using namespace std;
+
+// int main() {
+// 	int stairNum = 0;
+// 	cin >> stairNum;
+// 	vector<int> scores(stairNum, 0);
+// 	vector<pair<int, int> > scoreSum(stairNum, pair<int, int>(0, 0));
+
+// 	for (int i = 0; i < stairNum; i++)
+// 		cin >> scores[i];
+
+// 	if (stairNum > 1) {
+// 		scoreSum[0].first = scores[0];
+// 		scoreSum[0].second = scores[0];
+// 		scoreSum[1].first = scores[1] + scoreSum[0].first;
+// 		scoreSum[1].second = scores[1];
+
+// 		for (int i = 2; i < stairNum; i++) {
+// 			scoreSum[i].first = scores[i] + scoreSum[i - 1].second;
+// 			scoreSum[i].second = scores[i] + MAX(scoreSum[i - 2].first, scoreSum[i - 2].second);
+// 		}
+
+// 		cout << MAX(scoreSum[stairNum - 1].first, scoreSum[stairNum - 1].second) << endl;
+// 	}
+// 	else
+// 		cout << scores[0] << endl;
+
+// 	return 0;
+// }
+
 #include <iostream>
-#include <vector>
-#define MAX(X,Y) ((X)>(Y)?(X):(Y))
+#include <algorithm>
 using namespace std;
 
+int N = 0;
+int score[301];
+int dp[301][2];
+
 int main() {
-	int stairNum = 0;
-	cin >> stairNum;
-	vector<int> scores(stairNum, 0);
-	vector<pair<int, int> > scoreSum(stairNum, pair<int, int>(0, 0));
+    scanf("%d", &N);
+    for (int i = 1; i <= N; ++i) scanf("%d", &score[i]);
 
-	for (int i = 0; i < stairNum; i++)
-		cin >> scores[i];
+    dp[1][0] = score[1];
+    for (int k = 2; k <= N; ++k) {
+        dp[k][0] = max(dp[k-2][0], dp[k-2][1] + score[k-1]) + score[k];
+        dp[k][1] = dp[k - 1][0];
+    }
 
-	if (stairNum > 1) {
-		scoreSum[0].first = scores[0];
-		scoreSum[0].second = scores[0];
-		scoreSum[1].first = scores[1] + scoreSum[0].first;
-		scoreSum[1].second = scores[1];
-
-		for (int i = 2; i < stairNum; i++) {
-			scoreSum[i].first = scores[i] + scoreSum[i - 1].second;
-			scoreSum[i].second = scores[i] + MAX(scoreSum[i - 2].first, scoreSum[i - 2].second);
-		}
-
-		cout << MAX(scoreSum[stairNum - 1].first, scoreSum[stairNum - 1].second) << endl;
-	}
-	else
-		cout << scores[0] << endl;
-
-	return 0;
+    printf("%d\n", dp[N][0]);
+    return 0;
 }
