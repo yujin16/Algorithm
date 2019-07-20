565160cd818232970520a9d66e0738d16b883e01
yujin16 <bcd772000@naver.com>
2019-07-20 17:06:33 +0900

    1912번 연속합 https://yujin16.github.io/algorithm/algocode/2019/07/15/baekjoon-1912.html

--- Baekjoon/[1912]연속합.cpp
+++ Baekjoon/[1912]연속합.cpp
@@ -2,22 +2,42 @@
  * https://www.acmicpc.net/problem/1546
  */
 
+// #include <iostream>
+// #include <climits>
+// #define MAX(X, Y) ((X)>(Y))? (X):(Y);
+// using namespace std;
+
+// int main() {
+// 	int num = 0, currentMax = 0, resultMax = INT_MIN;
+// 	cin >> num;
+
+// 	for (int i = 0; i < num; i++) {
+// 		int newNum = 0;
+// 		cin >> newNum;
+// 		currentMax = MAX(currentMax + newNum, newNum);
+// 		resultMax = MAX(currentMax, resultMax);
+// 	}
+
+// 	cout << resultMax;
+// 	return 0;
+// }
+
+/*20190715 */
 #include <iostream>
 #include <climits>
-#define MAX(X, Y) ((X)>(Y))? (X):(Y);
+#include <algorithm>
 using namespace std;
 
+int N = 0, score = 0, curMax = 0, answer = INT_MIN;
+
 int main() {
-	int num = 0, currentMax = 0, resultMax = INT_MIN;
-	cin >> num;
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &score);
 
-	for (int i = 0; i < num; i++) {
-		int newNum = 0;
-		cin >> newNum;
-		currentMax = MAX(currentMax + newNum, newNum);
-		resultMax = MAX(currentMax, resultMax);
-	}
-
-	cout << resultMax;
-	return 0;
+        curMax = max(curMax + score, score);
+        answer = max(answer, curMax);
+    }
+    printf("%d\n", answer);
+    return 0;
 }
